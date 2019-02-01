1c30f61612e96835838cc6ac051956deacb52573
Patrick Lehmann <ptrcklehmann@gmail.com>
2019-02-02 00:44:27 +0100

    name and short_name updated

--- manifest-config.js
+++ manifest-config.js
@@ -1,8 +1,8 @@
 const theme = require('./src/theme');
 
 module.exports = {
-  name: 'Patrick Lehmann portfolio',
-  short_name: 'Patrick Lehmann portfolio',
+  name: 'ptrcklehmann - fullstack developer',
+  short_name: 'ptrcklehmann',
   start_url: '/',
   background_color: theme.colors.background,
   theme_color: theme.colors.primary,
